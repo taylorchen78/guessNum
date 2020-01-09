082ead35ba6cf31439cd4704168db429b866798e
Taylor Chen <s68175@gmail.com>
2020-01-09 23:59:39 +0800

    Add guess count

--- randomInt.py
+++ randomInt.py
@@ -2,12 +2,14 @@ import random
 
 num = random.randint(1, 100)
 bIsGuessRight = False
+guessCount = 0
 
 while bIsGuessRight == False:
 	guessNum = input("Guess num: ")
 	guessNum = int(guessNum)
+	guessCount += 1
 	if guessNum == num:
-		print("Correct")
+		print("Correct, total guess", guessCount, "count")
 		bIsGuessRight = True
 	elif guessNum > num:
 		print("Number is smaller")
